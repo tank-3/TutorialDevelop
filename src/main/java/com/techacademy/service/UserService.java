b2ec6c625a67cc915239c928b2284fca25f73021
tank-3 <taku3.na3.1415@gmail.com>
2023-03-22 18:49:47 +0900

    削除機能追加

---
+++ src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,41 @@
+package com.techacademy.service;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository repository) {
+        this.userRepository = repository;
+    }
+
+    /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+    
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+}
